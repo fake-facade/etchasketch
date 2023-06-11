8fc2d8fb503219f7b5ea7dd6803924fda2e9ca95
fake-facade <tatis-zapata.g@northeastern.edu>
2023-06-11 14:03:39 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const container = document.querySelector('div.container');
+
+let size = 16;
+
+function generator (size) {
+    for (let i = 0; i < size; i++){
+    const row = document.createElement('div');
+    container.appendChild(row);
+    row.classList.add('row-container');
+        for (let i = 0; i < size; i++){
+            const column = document.createElement('div');
+            row.appendChild(column);
+            column.classList.add('box');
+        };
+    };
+};
+
+generator(size); 
+
+const box = document.querySelectorAll("div.box");
+
+box.forEach((div) => {
+    div.addEventListener('mouseover', () => {
+        div.classList.toggle('hoveredBox');
+    });
+});
+
+function resetBoard() {
+    let input = prompt("How large would you like your grid?", size);
+    if (input > 100){
+        alert("Max size = 100x100!");
+        generator(100);
+    } else if (!Number.isInteger(size)){
+        alert("Numeric inputs only!");
+        generator(16);
+    } else {
+        generator(input);
+    }
+ };
