f844bc117dd5e9a847948c7f845f76c5577ef6ed
fake-facade <tatis-zapata.g@northeastern.edu>
2023-06-11 16:35:34 -0400

    fixed reset button function. removed borders

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const container = document.querySelector('div.container');
 
 let size = 16;
 
+const rowContainer = document.getElementsByClassName("row-container");
+const box = document.getElementsByClassName("box");
+
 function generator (size) {
     for (let i = 0; i < size; i++){
     const row = document.createElement('div');
@@ -13,20 +16,29 @@ function generator (size) {
             column.classList.add('box');
         };
     };
+    addEvent();
 };
+
 
 generator(size); 
 
-const box = document.querySelectorAll("div.box");
-
-box.forEach((div) => {
-    div.addEventListener('mouseover', () => {
-        div.classList.toggle('hoveredBox');
-    });
-});
+function addEvent (){
+        for (let i = 0; i < box.length; i++){
+        let div = box[i];
+        div.addEventListener('mouseover', () => {
+            div.classList.toggle('hoveredBox');
+        });
+    };
+};
 
 function resetBoard() {
     let input = prompt("How large would you like your grid?", size);
+    const length = rowContainer.length;
+
+    for (let i = 0; i < length; i++){
+        rowContainer[0].remove();
+    };
+
     if (input > 100){
         alert("Max size = 100x100!");
         generator(100);
@@ -35,5 +47,5 @@ function resetBoard() {
         generator(16);
     } else {
         generator(input);
-    }
+    };
  };
